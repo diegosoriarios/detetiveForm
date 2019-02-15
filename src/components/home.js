ef0cc62c4576b7a58590aa310bb24e4fc8b2a6b1
diego.rios <diego.rios@CTNAPPLE01YG8WN.local>
2019-02-15 18:44:09 -0200

    Fixing size

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../App.css'
 
+
 const pessoas = [
 	"Coronel Mostarda",
 	"Advogado",
@@ -31,15 +32,48 @@ const Home = () => (
 	<div className="home">
 		<h2>Pessoas</h2>
 		{pessoas.map(nome => (
-            <div id="pessoas" key={nome}><label>Talvez </label><input type="radio" name={nome} defaultChecked={true} /><label>Sim</label><input type="radio" name={nome} /><label>Não</label><input type="radio" name={nome} /> <span>{nome}</span></div>
+            <div id="pessoas" key={nome}>
+				<label>Talvez 
+					<input type="radio" name={nome} defaultChecked={true} value="Talvez" />
+				</label>
+				<label>Sim
+					<input type="radio" name={nome} value="Sim" />
+				</label>
+				<label>Não
+					<input type="radio" name={nome} value="Nao" />
+				</label>
+				<span>{nome}</span>
+			</div>
 		))}
 		<h2>Lugares</h2>
 		{lugares.map(nome => (
-			<div id="lugares" key={nome}><label>Talvez </label><input type="radio" name={nome} defaultChecked={true} /><label>Sim</label><input type="radio" name={nome} /><label>Não</label><input type="radio" name={nome} /> <span>{nome}</span></div>
+			<div id="lugares" key={nome}>
+				<label>Talvez 
+					<input type="radio" name={nome} defaultChecked={true} value="Talvez" />
+				</label>
+				<label>Sim
+					<input type="radio" name={nome} value="Sim" />
+				</label>
+				<label>Não
+					<input type="radio" name={nome} value="Nao" />
+				</label>
+				<span>{nome}</span>
+			</div>
 		))}
 		<h2>Armas</h2>
 		{arma.map(nome => (
-			<div id="armas" key={nome}><label>Talvez </label><input type="radio" name={nome} defaultChecked={true} /><label>Sim</label><input type="radio" name={nome} /><label>Não</label><input type="radio" name={nome} /> <span>{nome}</span></div>
+			<div id="armas" key={nome}>
+				<label>Talvez 
+					<input type="radio" name={nome} defaultChecked={true} value="Talvez" />
+				</label>
+				<label>Sim
+					<input type="radio" name={nome} value="Sim" />
+				</label>
+				<label>Não
+					<input type="radio" name={nome} value="Nao" />
+				</label>
+				<span>{nome}</span>
+			</div>
 		))}
 	</div>
 );
