4f2697cde176158a06e48d17fcfca6e101e670b7
diegosoriarios <diegosoriarios@gmail.com>
2019-02-22 15:05:34 -0300

    Map

--- src/App.js
+++ src/App.js
@@ -28,8 +28,8 @@ const customStyles = {
 	  bottom                : 'auto',
 	  marginRight           : '-50%',
 		transform             : 'translate(-50%, -50%)',
-		width									:	'50%',
-		height								:	'50%',
+		width									:	'90%',
+		height								:	'80%',
 		textAlign							: 'center',
 	}
 };
